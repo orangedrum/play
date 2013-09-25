5f58927fbb5773e490dcf26fba7b747d98bbd3e2
orangedrum <jean.kaluza@orangedrum.com>
2013-09-25 09:55:08 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Play::Application.config.secret_key_base = '9cf59af8910751b01291981031682af5ebd211ab16e5b5488cd46a21a5d188750c423fcdb9bccc2e5a346360d4863b9b86df4a2f8fb0e49ba317ba7982cf1877'
